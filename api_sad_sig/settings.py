330061400458cbed5f20adacd5f50d36276976be
Agung Wiyono <wiyonoagung1@gmail.com>
2020-12-17 06:51:36 +0700

    Set up cors whitelist

--- api_sad_sig/settings.py
+++ api_sad_sig/settings.py
@@ -31,6 +31,12 @@ SECRET_KEY = env.str("SECRET_KEY")
 DEBUG = env.bool("DEBUG", default=True)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", [])
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+    'http://localhost:8000',
+]
+# CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Application definition
@@ -46,11 +52,13 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "dynamic_rest",
     "rest_framework",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
