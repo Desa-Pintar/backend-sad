9d156ff1c2b32f97902bebf90bc1b47da5332f35
invarii <64796357+invarii@users.noreply.github.com>
2021-04-13 14:49:09 +0700

    print absensi (#325)

--- v1/views.py
+++ v1/views.py
@@ -57,7 +57,6 @@ class PegawaiViewSet(CustomView):
     serializer_class = PegawaiSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-
 class BatasDesaViewSet(CustomView):
     queryset = BatasDesa.objects.all().order_by("id")
     serializer_class = BatasDesaSerializer
@@ -1393,6 +1392,43 @@ class AbsensiViewSet(DynamicModelViewSet):
     queryset = Absensi.objects.all().order_by("-id")
     serializer_class = AbsensiSerializer
     permission_classes = [AllowAny]
+
+    @action(detail=False, methods=["get"])
+    def ekspor(self, request):
+        extras = {
+            "Nama": "pegawai.nama",
+            "Jabatan": "pegawai.jabatan",
+            "Tanggal/Jam Masuk": "jam_masuk",
+            "Tanggal/Jam Keluar": "jam_keluar"
+        }
+        data = (
+            self.get_queryset()
+            .extra(select=extras, tables=("pegawai",))
+            .values(*extras.keys())
+            .all()
+        )
+
+        workbook = Workbook()
+        sheet = workbook.active
+
+        headers = [i for i in extras.keys()]
+        for index, value in enumerate(headers):
+            sheet.cell(row=1, column=index + 1).value = value
+
+        for i, x in enumerate(data):
+            for idx, value in enumerate(x.values()):
+                sheet.cell(row=i + 2, column=idx + 1).value = value
+
+        output = BytesIO()
+        workbook.save(output)
+        response = HttpResponse(
+            output.getvalue(),
+            content_type="application/vnd.ms-excel",
+        )
+        response[
+            "Content-Disposition"
+        ] = 'attachment; filename="DataPresensi.xlsx"'
+        return response
 
 
 class AlasanIzinViewSet(DynamicModelViewSet):
